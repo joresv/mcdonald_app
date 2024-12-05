db9110c4e097f533c4ee1cf7cb86aac87b090f73
Jores Tala <jorestala16@gmail.com>
2024-12-05 17:00:25 +0100

    carousel in progress

--- lib/pages/carousel/carousel.page.dart
+++ lib/pages/carousel/carousel.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mcdonald_app/utils/app.images.dart';
 import 'package:mcdonald_app/widgets/curved.carousel.dart';
 
 class CarouselPage extends StatelessWidget {
@@ -9,20 +10,72 @@ class CarouselPage extends StatelessWidget {
     return Scaffold(
       body: CurvedCarousel(
         items: [
-          Container(
-            color: Colors.red,
-            child: Text('Item 1'),
+          _buildCarouselItem(
+            'Title',
+            '25.0',
           ),
-          Container(
-            color: Colors.green,
-            child: Text('Item 2'),
+          _buildCarouselItem(
+            'Title',
+            '25.0',
           ),
-          Container(
-            color: Colors.yellow,
-            child: Text('Item 3'),
+          _buildCarouselItem(
+            'Title',
+            '25.0',
           ),
         ],
       ),
     );
   }
+}
+
+Widget _buildCarouselItem(String title, String price) {
+  return Container(
+    width: 300,
+    height: 400,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          blurRadius: 10,
+          offset: const Offset(0, 5),
+        ),
+      ],
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                price,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Image.asset(
+            AppImages.glass1,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ],
+    ),
+  );
 }
